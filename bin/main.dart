5e25af8ea7c44f77d4af2070268f19389b07c991
Germain-L <germain.leignel@gmail.com>
2020-02-03 18:19:12 +0000

    binaries created

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,5 @@
-import 'package:algorithms_and_datastructures/algorithms_and_datastructures.dart' as algorithms_and_datastructures;
+import 'package:algorithms_and_datastructures/datastructures/stack.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${algorithms_and_datastructures.calculate()}!');
+
 }
