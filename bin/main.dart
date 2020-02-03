1f400775c4d9a75d97fe45d52941476d60e839ff
Germain-L <germain.leignel@gmail.com>
2020-02-03 18:09:37 +0000

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:algorithms_and_datastructures/algorithms_and_datastructures.dart' as algorithms_and_datastructures;
+
+void main(List<String> arguments) {
+  print('Hello world: ${algorithms_and_datastructures.calculate()}!');
+}
