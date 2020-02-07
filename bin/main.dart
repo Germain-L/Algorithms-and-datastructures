d4e66e03ba5f076dba95d75ec32e1cdbb183be57
Germain-L <germain.leignel@gmail.com>
2020-02-07 20:36:39 +0000

    refactoring

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,8 @@
-import 'package:algorithms_and_datastructures/algorithms/binary_search.dart';
-import 'package:algorithms_and_datastructures/algorithms/linear_search.dart';
+import 'package:algorithms_and_datastructures/algorithms/search/binary_search.dart';
+import 'package:algorithms_and_datastructures/algorithms/search/linear_search.dart';
 import 'package:algorithms_and_datastructures/datastructures/queue.dart';
 import 'package:algorithms_and_datastructures/datastructures/stack.dart';
+
 void main() {
   List it = List();
   for (int i = 0; i < 50; i++) {
